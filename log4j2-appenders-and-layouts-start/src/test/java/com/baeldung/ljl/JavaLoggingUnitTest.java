6bcae9638055755906e3340b39a38114a4695305
Baeldung CI User <baeldungci@baeldung.com>
2025-10-17 17:06:48 +0000

    Merge pull request #15 from hardikSinghBehl/codebase/log4j2-appenders-and-layouts-COURSE-249
    codebase/log4j2-appenders-and-layouts [COURSE-249]

---
+++ log4j2-appenders-and-layouts-start/src/test/java/com/baeldung/ljl/JavaLoggingUnitTest.java
@@ -0,0 +1,9 @@
+package com.baeldung.ljl;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+class JavaLoggingUnitTest {
+    private static final Logger logger = LogManager.getLogger(JavaLoggingUnitTest.class);
+
+}
